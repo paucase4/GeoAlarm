f81c55d94920fff93a88365a27a34dad4490015d
Pau Case <paucase4@gmail.com>
2025-12-19 12:42:10 +0100

    Initial Commit

---
+++ geoAlarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  geoAlarm
+//
+//  Created by user282935 on 12/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
